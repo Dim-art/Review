b527fa47b6a352de20295342e74c57d274eeab1c
DimArt <midiline.art@gmail.com>
2017-06-28 13:09:55 +0300

    sf-menu

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,34 @@
-var gulp       	 = require('gulp'),
-		sass         = require('gulp-sass'),
-		autoprefixer = require('gulp-autoprefixer'),
-		cleanCSS     = require('gulp-clean-css'),
-		rename       = require('gulp-rename'),
-		browserSync  = require('browser-sync').create(),
-		concat       = require('gulp-concat'),
-		uglify       = require('gulp-uglify');
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	autoprefixer = require('gulp-autoprefixer'),
+	cleanCSS = require('gulp-clean-css'),
+	rename = require('gulp-rename'),
+	browserSync = require('browser-sync').create(),
+	concat = require('gulp-concat'),
+	uglify = require('gulp-uglify');
 
-gulp.task('browser-sync', ['styles', 'scripts'], function() {
-		browserSync.init({
-				server: {
-						baseDir: "./app"
-				},
-				notify: false
-		});
+gulp.task('browser-sync', ['styles', 'scripts'], function () {
+	browserSync.init({
+		server: {
+			baseDir: "./app"
+		},
+		notify: false
+	});
 });
 
 gulp.task('styles', function () {
 	return gulp.src('sass/*.sass')
-	.pipe(sass({
-		includePaths: require('node-bourbon').includePaths
-	}).on('error', sass.logError))
-	.pipe(rename({suffix: '.min', prefix : ''}))
-	.pipe(autoprefixer({browsers: ['last 15 versions'], cascade: false}))
-	.pipe(cleanCSS())
-	.pipe(gulp.dest('app/css'))
-	.pipe(browserSync.stream());
+		.pipe(sass({
+			includePaths: require('node-bourbon').includePaths
+		}).on('error', sass.logError))
+		.pipe(rename({ suffix: '.min', prefix: '' }))
+		.pipe(autoprefixer({ browsers: ['last 15 versions'], cascade: false }))
+		.pipe(cleanCSS())
+		.pipe(gulp.dest('app/css'))
+		.pipe(browserSync.stream());
 });
 
-gulp.task('scripts', function() {
+gulp.task('scripts', function () {
 	return gulp.src([
 		'./app/libs/modernizr/modernizr.js',
 		'./app/libs/jquery/jquery-1.11.2.min.js',
@@ -36,7 +36,7 @@ gulp.task('scripts', function() {
 		'./app/libs/animate/animate-css.js',
 		'./app/libs/plugins-scroll/plugins-scroll.js'
 		'./app/libs/superfish/dist/js/superfish.min.js'
-		])
+	])
 		.pipe(concat('libs.js'))
 		// .pipe(uglify()) //Minify libs.js
 		.pipe(gulp.dest('./app/js/'));
